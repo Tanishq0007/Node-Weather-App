a5c6ab4c4ab7c3b27883e1ba9fa79a1599b518f2
Tanishq0007 <tanishq16verma@gmail.com>
2020-05-23 19:58:54 +0530

    Removed Console Log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side java script')
-
 const weather_form = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
